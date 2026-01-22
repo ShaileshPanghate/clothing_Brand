e6f6b3426104d9022477b3e9df1f01c4150900da
Bluebird0223 <ramteke4101971@gmail.com>
2026-01-22 11:20:38 +0530

    backend added

--- src/app/components/Loader.jsx
+++ src/app/components/Loader.jsx
@@ -1,5 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+import logo from '../../../public/images/logo/logo.png';
 
 export default function Loader({ fullScreen = false }) {
   return (
@@ -15,11 +17,11 @@ export default function Loader({ fullScreen = false }) {
         {/* Brand Text / Initial */}
         <div className="absolute inset-0 flex items-center justify-center">
           <motion.span
-            animate={{ opacity: [0.3, 1, 0.3] }}
+            animate={{ opacity: [0.3, 1, 0.3], rotate: -360 }}
             transition={{ repeat: Infinity, duration: 1.5 }}
             className="text-xs font-bold uppercase tracking-widest text-black"
           >
-            G
+            <Image src={logo} alt="logo" width={50} height={50} />
           </motion.span>
         </div>
       </div>
